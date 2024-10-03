cd39f2eb079a79bf96c41ab8dbe8ba8018413800
Yassine Errakkas <yassine.errakkas11@gmail.com>
2024-10-03 16:57:19 +0100

    adapte the repo to leehub extension

--- LeetCode/Two Sum/Solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        D = {}
-        for i in range(len(nums)):
-            if target - nums[i] in D:
-               return [D[target - nums[i]],i]
-            D[nums[i]] = i
