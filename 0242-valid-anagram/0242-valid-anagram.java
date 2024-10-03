7b29d6f05c6c2d6b10f1c004c122eca253415b15
Yassine Errakkas <69655245+yassineerrakkas@users.noreply.github.com>
2024-10-03 16:45:16 +0100

    Time: 5 ms (53.09%), Space: 42.9 MB (80.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,10 +3,10 @@ class Solution {
         if(s.length() != t.length()) return false;
         int [] acc = new int[26];
         for(char c : s.toCharArray()){
-            acc[c-'a'] += 1;
+            acc[c-'a']++;
         }
         for(char c : t.toCharArray()){
-            acc[c-'a'] -=1;
+            acc[c-'a']--;
         }
         for(int i : acc){
             if(i != 0) return false;
