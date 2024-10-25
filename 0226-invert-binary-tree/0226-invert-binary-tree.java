23b110a95759bc7a1336bef535175b331b832ea3
Yassine Errakkas <69655245+yassineerrakkas@users.noreply.github.com>
2024-10-25 02:11:09 +0100

    Time: 0 ms (100%), Space: 41.1 MB (36.28%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,12 +15,13 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null) return root;
-        TreeNode temp = root.right;
-        root.right = root.left;
-        root.left = temp;
-        invertTree(root.right);
-        invertTree(root.left);
+         if(root == null){
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
         return root;
     }
 }
