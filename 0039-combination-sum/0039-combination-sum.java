0afc5d7a7e35ded5bd7219a88afcc2dbf3f324d1
Yassine Errakkas <69655245+yassineerrakkas@users.noreply.github.com>
2024-10-27 05:17:09 +0100

    Time: 4 ms (16.97%), Space: 45.5 MB (6.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,11 +1,11 @@
 class Solution {
+    List<List<Integer>> list = new ArrayList();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList();
         List<Integer> temp = new ArrayList();
-        helper(candidates,target,temp,ans,0);
-        return ans;
+        helper(candidates,target,temp,0);
+        return list;
     }
-    public void helper(int[] candidates, int target,List<Integer> temp,List<List<Integer>> list,int i){
+    public void helper(int[] candidates, int target,List<Integer> temp,int i){
         if(target < 0 || i > candidates.length-1) return;
         if(target == 0) {
             list.add(temp);
@@ -13,7 +13,7 @@ class Solution {
         }
         List<Integer> tt = new ArrayList(temp);
         tt.add(candidates[i]);
-        helper(candidates,target - candidates[i],tt,list,i);
-        helper(candidates,target,temp,list,i+1);
+        helper(candidates,target - candidates[i],tt,i);
+        helper(candidates,target,temp,i+1);
     }
 }
