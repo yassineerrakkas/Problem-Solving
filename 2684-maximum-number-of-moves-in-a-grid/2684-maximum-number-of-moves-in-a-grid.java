11a0c44cb3e8b0f8ec810c9f281cf734e3f1d758
Yassine Errakkas <69655245+yassineerrakkas@users.noreply.github.com>
2024-10-29 05:46:37 +0100

    Time: 5 ms (100%), Space: 56.3 MB (60.15%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
@@ -1,25 +1,23 @@
 class Solution {
+    Integer[][] dp;
+    int m, n;
+    int[][] grid;
     public int maxMoves(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] dp = new int[n][m];
-        
-        int[] directions = {-1, 0, 1};
-        for (int j = m - 2; j >= 0; j--) {
-            for (int i = 0; i < n; i++) {
-                for (int d : directions) {
-                    int newRow = i + d;
-                    if (newRow >= 0 && newRow < n && grid[i][j] < grid[newRow][j + 1]) {
-                        dp[i][j] = Math.max(dp[i][j], dp[newRow][j + 1] + 1);
-                    }
-                }
-            }
+        m = grid.length; n = grid[0].length;
+        this.grid = grid;
+        dp = new Integer[m][n];
+        var res = 0;
+        for(var i=0; i<m; i++){
+            res = Math.max(memoSolve(i, 0), res);
         }
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            result = Math.max(result, dp[i][0]);
-        }
-
-        return result;
+        return res;
+    }
+    int memoSolve(int i, int j){
+        if(dp[i][j] != null) return dp[i][j];
+        var res = 0;
+        if(i-1>=0 && j+1<n && grid[i-1][j+1] > grid[i][j]) res = 1 + memoSolve(i-1, j+1);
+        if(j+1<n && grid[i][j+1] > grid[i][j]) res = Math.max(res, 1 + memoSolve(i, j+1));
+        if(i+1<m && j+1<n && grid[i+1][j+1] > grid[i][j]) res = Math.max(res, 1 + memoSolve(i+1, j+1));
+        return dp[i][j] = res;
     }
 }
