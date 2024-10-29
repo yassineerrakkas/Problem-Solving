675cd4cbff9f34e4dfd62c79eba7b9bad4185e43
Yassine Errakkas <69655245+yassineerrakkas@users.noreply.github.com>
2024-10-29 05:48:45 +0100

    Time: 5 ms (100%), Space: 56.7 MB (23.91%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
@@ -14,7 +14,7 @@ class Solution {
     }
     int memoSolve(int i, int j){
         if(dp[i][j] != null) return dp[i][j];
-        var res = 0;
+        int res = 0;
         if(i-1>=0 && j+1<n && grid[i-1][j+1] > grid[i][j]) res = 1 + memoSolve(i-1, j+1);
         if(j+1<n && grid[i][j+1] > grid[i][j]) res = Math.max(res, 1 + memoSolve(i, j+1));
         if(i+1<m && j+1<n && grid[i+1][j+1] > grid[i][j]) res = Math.max(res, 1 + memoSolve(i+1, j+1));
