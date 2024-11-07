d7818b0929cc22aae3176a530f17327b191cf436
Yassine Errakkas <69655245+yassineerrakkas@users.noreply.github.com>
2024-11-07 17:08:37 +0100

    Time: 36 ms (12.7%), Space: 57.1 MB (12.75%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i = 0; i < 24; i++){
             int count = 0;
             for(int j = 0; j < candidates.length; j++){
-                if((candidates[j] & 1) == 1) count++;
+                if(candidates[j] != 0 && (candidates[j] & 1) == 1) count++;
                 candidates[j] = candidates[j] >> 1;
             }
             res = Math.max(res,count);
