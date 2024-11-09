0f92b95620495b58d8900e6a6b37403f4d36367f
Yassine Errakkas <69655245+yassineerrakkas@users.noreply.github.com>
2024-11-09 13:05:49 +0100

    Time: 2 ms (100%), Space: 58.7 MB (71.9%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
@@ -1,15 +1,19 @@
 class Solution {
     public int[] getMaximumXor(int[] nums, int maximumBit) {
         int n = nums.length;
-        int [] xor = new int[n];
-        xor[n-1] = nums[0];
-        for(int i = 1; i < n; i++){
-            xor[n-1-i] = xor[n-i] ^ nums[i];
+        int xorr = nums[0];
+        int maxXor = (1 << maximumBit) - 1;
+
+        for (int i = 1; i < n; i++) {
+            xorr ^= nums[i];
         }
-        int mask = (int)Math.pow(2,maximumBit) -1;
-        for(int i = 0; i<n; i++){
-            xor[i] = ~xor[i] & mask;
+
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = xorr ^ maxXor;
+            xorr ^= nums[n - 1 - i];
         }
-        return xor;
+
+        return ans;
     }
 }
