f69cae8efff2b09f18388b90ae3429c65328c43e
Yassine Errakkas <69655245+yassineerrakkas@users.noreply.github.com>
2024-12-01 04:16:49 +0100

    Time: 2 ms (75.11%), Space: 52.1 MB (10.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,12 +5,9 @@
  */
 var twoSum = function(nums, target) {
     let map = new Map();
-    let ans = Array(2);
     for(let i = 0; i < nums.length; i++){
         if(map.has(nums[i])){
-            ans[0] = i;
-            ans[1] = map.get(nums[i]);
-            return ans;
+            return [i,map.get(nums[i])];
         }
         map.set(target-nums[i],i);
     }
