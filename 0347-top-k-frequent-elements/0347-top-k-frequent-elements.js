cb32c5c521605f5b8e387f93feebfd65d1ebfd4e
Yassine Errakkas <69655245+yassineerrakkas@users.noreply.github.com>
2024-12-01 15:43:30 +0100

    Time: 11 ms (76.18%), Space: 53.8 MB (63.54%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const map = new Map();
+    for(const num of nums){
+        map.set(num, (map.get(num) || 0 ) + 1);
+    }
+    return Array.from(map.entries())
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, k)
+        .map(entry => entry[0]);
+};
